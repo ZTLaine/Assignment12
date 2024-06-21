9229a86b06a24f31334d0f313725ea2877ee4e92
Zack Laine <rumtehk_uh@yahoo.com>
2024-06-21 16:29:10 -0400

    Updated the schema a bit and finished entering in the provided data
    Query for the transactions' pizzas and insert the results to transactions.total_cost next

--- Assignment12.sql
+++ Assignment12.sql
@@ -1,12 +1,5 @@
 CREATE SCHEMA IF NOT EXISTS `pizza_restaurant`;
 
-CREATE TABLE IF NOT EXISTS `pizza_restaurant`.`pizzas` (
-	`pizza_id` INT NOT NULL AUTO_INCREMENT,
-	`type` VARCHAR(50) NOT NULL,
-	`price` DECIMAL(5,2) NOT NULL,
-	PRIMARY KEY (`pizza_id`)
-);
-
 CREATE TABLE IF NOT EXISTS `pizza_restaurant`.`customers` (
 	`customer_id` INT NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(150) NOT NULL,
@@ -18,8 +11,18 @@ CREATE TABLE IF NOT EXISTS `pizza_restaurant`.`transactions` (
 	`transaction_id` INT NOT NULL AUTO_INCREMENT,
 	`customer_id` INT NOT NULL,
 	`order_datetime` DATETIME NOT NULL,
+    `total_price` DECIMAL(6,2) ZEROFILL,
 	FOREIGN KEY (`customer_id`) REFERENCES `pizza_restaurant`.`customers` (`customer_id`),
 	PRIMARY KEY (`transaction_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `pizza_restaurant`.`pizzas` (
+	`pizza_id` INT NOT NULL AUTO_INCREMENT,
+    `transaction_id` INT NOT NULL,
+	`type` VARCHAR(50) NOT NULL,
+	`price` DECIMAL(5,2) NOT NULL,
+    FOREIGN KEY (`transaction_id`) REFERENCES `pizza_restaurant`.`transactions` (`transaction_id`),
+	PRIMARY KEY (`pizza_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `pizza_restaurant`.`customer_pizza` (
@@ -36,13 +39,6 @@ CREATE TABLE IF NOT EXISTS `pizza_restaurant`.`customer_pizza` (
 	FOREIGN KEY (`pizza_id`) REFERENCES `pizza_restaurant`.`pizzas` (`pizza_id`)
 );
 
-INSERT INTO `pizza_restaurant`.`pizzas` (`type`, `price`)
-VALUES 
-	('Pepperoni & Cheese', 7.99),
-    ('Vegetarian', 9.99),
-    ('Meat Lovers', 14.99),
-    ('Hawaiian', 12.99);
-
 INSERT INTO `pizza_restaurant`.`customers` (`name`, `phone_num`)
 VALUES 
 	('Trevor Page', '226-555-4982'),
@@ -50,7 +46,24 @@ VALUES
     
 INSERT INTO `pizza_restaurant`.`transactions` (`customer_id`, `order_datetime`)
 VALUES 
-	(1, '2023-9-10 9:47:00'),
+	(1, '2023-9-10 09:47:00'),
     (2, '2023-9-10 13:20:00'),
-    (1, '2023-9-10 9:47:00'),
+    (1, '2023-9-10 09:47:00'),
     (2, '2023-10-10 10:37:00');
+    
+INSERT INTO `pizza_restaurant`.`pizzas` (`transaction_id`,`type`, `price`)
+VALUES 
+	(1, 'Pepperoni & Cheese', 7.99),
+    (1, 'Meat Lovers', 14.99),
+	(2, 'Vegetarian', 9.99),
+    (2, 'Meat Lovers', 14.99),
+    (2, 'Meat Lovers', 14.99),
+    (3, 'Meat Lovers', 14.99),
+    (3, 'Hawaiian', 12.99),
+    (4, 'Vegetarian', 9.99),
+    (4, 'Vegetarian', 9.99),
+    (4, 'Vegetarian', 9.99),
+    (4, 'Hawaiian', 12.99);
+    
+
+    
